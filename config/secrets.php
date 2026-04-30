e75569f816cfc07a5814e72f001007e0dab59de7
FrehiwetZ <papsye30@gmail.com>
2026-04-29 21:40:22 -0900

    Add Google OAuth client ID placeholder for social login

--- config/secrets.php
+++ config/secrets.php
@@ -3,4 +3,6 @@
 define('STRIPE_SECRET_KEY', 'sk_test_YOUR_STRIPE_SECRET_KEY');
 define('STRIPE_PUBLISHABLE_KEY', 'pk_test_YOUR_STRIPE_PUBLISHABLE_KEY');
 
+// Google Identity Services (https://console.cloud.google.com/)
+define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com');
 ?>
