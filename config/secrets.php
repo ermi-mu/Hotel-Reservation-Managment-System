430824900cca8db664f6292716e97bec089c0088
FrehiwetZ <papsye30@gmail.com>
2026-04-29 21:40:59 -0900

    Add Facebook App ID placeholder for social login

--- config/secrets.php
+++ config/secrets.php
@@ -5,4 +5,7 @@ define('STRIPE_PUBLISHABLE_KEY', 'pk_test_YOUR_STRIPE_PUBLISHABLE_KEY');
 
 // Google Identity Services (https://console.cloud.google.com/)
 define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com');
+
+// Facebook Developers (https://developers.facebook.com/)
+define('FACEBOOK_APP_ID', 'YOUR_FACEBOOK_APP_ID');
 ?>
